22c7d9fb2bd3c0d5d40b09d1ba0e31b16c011bbf
DELL <DELL@DESKTOP-O0SDLDA>
2025-08-19 21:57:42 +0530

    I have completed assignment 3 program

---
+++ C2TC_CoreJavaAssignment/src/assignment_3/Book.java
@@ -0,0 +1,20 @@
+package assignment_3;
+
+public class Book extends Item {
+    private String author;
+
+    public Book(int id, String title, String author) {
+        super(id, title);
+        this.author = author;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book: [ID: " + super.toString() + ", Title: " + getTitle() + ", Author: " + author + "]");
+    }
+
+    @Override
+    public String toString() {
+        return getTitle() + " by " + author;
+    }
+}
